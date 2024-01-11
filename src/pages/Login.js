3a9bb699b3ee109de142c849b88ca0f55dc1a160
Siddhant51 <siddhantsonawane37@gmail.com>
2024-01-11 22:16:54 +0530

    Loading Bar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,45 +1,53 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import LoadingBar from "react-top-loading-bar";
 import { BASE_URL } from "../components/BaseUrl";
-
-// const BASE_URL = "https://noteit-api-b5ly.onrender.com";
-// const BASE_URL = "http://localhost:3001";
 
 const Login = ({ setToken }) => {
   const navigate = useNavigate();
+  const loadingBar = useRef(null);
 
   const [formData, setFormData] = useState({
     email: "sid@gmail.com",
     password: "123456",
   });
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    axios
-      .post(`${BASE_URL}/login`, formData)
-      .then((res) => {
-        console.log(res);
-        setToken(res.data.token);
-        localStorage.setItem("token", res.data.token);
-        localStorage.setItem("theme", "Dark");
-        toast.success("Login successful!");
-        setTimeout(() => {
-          navigate("/", { replace: true });
-        }, 4000);
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error("Invalid credentials. Please try again.");
-      });
+    try {
+      // Show the loading bar
+      loadingBar.current.continuousStart();
+
+      const res = await axios.post(`${BASE_URL}/login`, formData);
+
+      // Hide the loading bar
+      loadingBar.current.complete();
+
+      console.log(res);
+      setToken(res.data.token);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("theme", "Dark");
+      toast.success("Login successful!");
+      setTimeout(() => {
+        navigate("/", { replace: true });
+      }, 4000);
+    } catch (err) {
+      // Hide the loading bar on error
+      loadingBar.current.complete();
+
+      console.log(err);
+      toast.error("Invalid credentials. Please try again.");
+    }
   };
 
   return (
     <>
       <ToastContainer />
+      <LoadingBar color="#f11946" ref={loadingBar} />
       <div className="header">NoteIt</div>
       <div className="body">
         <form className="outer" onSubmit={handleSubmit}>
