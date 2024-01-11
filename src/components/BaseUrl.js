9c2c0b749e14e61b7f511cd8ae7e3333e037c3ed
Siddhant51 <siddhantsonawane37@gmail.com>
2024-01-11 22:17:43 +0530

    Loading Bar

--- src/components/BaseUrl.js
+++ src/components/BaseUrl.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://noteit-api-b5ly.onrender.com";
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://noteit-api-b5ly.onrender.com";
+// export const BASE_URL = "http://localhost:3001";
